39e72b185aeaa145864a1f0f715d77c3cec09517
GreDDySS <misha.sudos@yandex.ru>
2024-07-15 13:31:55 +0800

    Static HEADER loading on all pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,13 @@
 <script lang="ts">
+    import Header from "~/components/Header.vue"
+    export default {
+        components: Header
+    }
 </script>
 
 <template>
-    <header class="flex items-center justify-between bg-background px-4 py-3 shadow-sm border-b">
-        <div class="flex items-center gap-2">
-            <a href="#" class="flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3" />
-                </svg>
-                <h1 class="text-lg font-semibold">GreDDBot</h1>
-            </a>
-            <a href="#" class="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium hover:text-accent-foreground">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
-                </svg>
-                Dashboard
-            </a>
-            <a class="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium hover:text-accent-foreground" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                </svg>
-                Logs
-            </a>
-        </div>
-            <a target="_blank" class="inline-flex items-center gap-2 rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground transition-colors hover:bg-primary/90 focus:outline-none focus:ring-1 focus:ring-primary/50" href="https://github.com/GreDDySS">
-                <img src="/public/github.png">
-                GitHub
-            </a>
-    </header>
+    <div>
+        <Header />
+        <Nuxt />
+    </div>
 </template>
