bfa2ace4a07c28df3415307a1bac2099fc07284b
GreDDySS <misha.sudos@yandex.ru>
2024-07-28 02:02:03 +0800

    There are now logs for each channel

--- pages/logs.vue
+++ pages/logs.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
   import { ScrollArea } from '~/components/ui/scroll-area'
+  import {ref, onMounted} from 'vue'
 
-  const channels = await $fetch('/api/getChannel')
+  interface Channel {
+    id: number
+    name: string
+  }
 
-  const logs = await $fetch('/api/getMessage')
+  interface Log {
+    id: number
+    username: string
+    color: string
+    message: string
+    date: string
+  }
+
+  const channels = ref<Channel[]>([])
+  const logs = ref<Log[]>([])
+
+  const fetchChannels = async () => {
+    channels.value = await $fetch('/api/getChannel')
+  }
+  
+  const fetchMessages = async (channelId: number) => {
+    logs.value = await $fetch(`/api/getMessage`, {
+      params: {id: channelId}
+    });
+  };
+
+  onMounted(() => {
+    fetchChannels()
+  })
 </script>
 
 <template>
@@ -15,7 +42,10 @@
           <ScrollArea class="h-[500px]  w-full rounded-md ">
             <div class="p-1">
               <div v-for="channel in channels" :key="channel.id">
-                <button class="text-md font-semibold w-full rounded-sm bg-popover p-2 m-1.5">
+                <button 
+                class="text-md font-semibold w-full rounded-sm bg-popover p-2 m-1.5"
+                @click="fetchMessages(channel.id)"
+                >
                   {{ channel.name }}
                 </button>
               </div>
@@ -25,15 +55,32 @@
         <!-- Block Log message -->
         <div class="col-span-3 bg-card p-5 rounded-lg">
           <h4 class="text-3xl text-center mb-2">Logs</h4>
-          <ScrollArea class="h-[450px] w-full rounded-md border">
-            <div class="p-1">
-              <div v-for="log in logs" :key="log.id">
-                <p><span class="text-neutral-600">{{ log.date }}</span> <span class="text-md" :style="{color: log.color}">{{ log.username }}:</span> {{ log.message }}</p>
+          <div v-if="logs.length === 0" class="h-full flex items-center justify-center leading-8 text-neutral-600 text-center">
+
+            <ul class=" bg-neutral-900 p-4 rounded-lg">
+              <li>
+                <div>
+                  <Icon name="majesticons:chat-2-line" size="48px"/>
+                </div>
+              </li>
+              <li>Выберите канал, чтобы увидеть сообщения.</li>
+              <li>Если нет сообщений, выберите другой канал.</li>
+              
+            </ul>
+          </div>
+          <div v-else>
+            <ScrollArea class="h-[450px] w-full rounded-md border">
+              <div class="p-1">
+                <div v-for="log in logs" :key="log.id">
+                  <p>
+                    <span class="text-neutral-600">{{ log.date }}</span> <span class=" font-semibold" :style="{color: log.color}">{{ log.username }}:</span>
+                    {{ log.message }}
+                  </p>
+                </div>
               </div>
-            </div>
-          </ScrollArea>
+            </ScrollArea>
+          </div>
         </div>
       </div>
-
     </div>
 </template>
--- server/api/getMessage.ts
+++ server/api/getMessage.ts
@@ -1,11 +1,14 @@
 import { withPG } from "../plugins/pg";
 
 export default eventHandler(async (event) => {
+  const channel = getQuery(event)
+
   return await withPG(event, async (dbClient) => {
-    const message = await dbClient.query(`SELECT * from "channelLogs"."122007563"`)
+   
+   const message = await dbClient.query(`SELECT * from "channelLogs"."${channel.id}"`)
     const result = message.rows.map((item) => {
       const date = new Date(item.date);
-      const formattedDate = date.toLocaleString('en-US', {
+      const formattedDate = date.toLocaleString('ru-ru', {
         month: '2-digit',
         day: '2-digit',
         hour: '2-digit',
