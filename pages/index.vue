69567958b1a1e4c7fd98f87865e961be414e7620
GreDDySS <misha.sudos@yandex.ru>
2025-01-07 21:16:11 +0800

    Main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,23 @@
-<script>
-export default {
-  name: 'HomePage'
-}
+<script setup lang="ts">
+  import {Button} from "../components/ui/button"
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center m-20">
-    <h1 class="text-4xl font-bold mb-8">Welcome</h1>
-    <div class="flex flex-col items-center justify-center">
-        <div class="flex flex-col gap-4 p4">
-            <div class="flex-[2] bg-card rounded-md p-4">
-                <div class="flex items-center gap-10 pb-5 leading-8">
-                    <img src="https://cdn.7tv.app/emote/60bfb67aa2610d253072f14a/4x.webp" height="160px" width="160px">
-                    <div>
-                        <h2 class="text-2xl font-bold">Twitch Bot</h2>
-                        <ul class="text-muted-foreground list-disc">
-                            <li>Этот бот предназначен для работы с чатом Twitch.</li>
-                            <li>Он позволяет автоматизировать различные задачи, выполнение команд чата.</li>
-                            <li>Бот также ведёт логирование сообщений и тесно интегрирован с базой данных.</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="flex flex-row gap-4">
-                    <a href="/commands" class="bg-popover rounded-md p-4 gap-4 flex-1 flex items-center justify-center hover:bg-accent hover:text-accent-foreground">
-                        <Icon name="grommet-icons:command" size="24"/>
-                        Commands
-                    </a>
-                    <a href="/logs" class="bg-popover rounded-md p-4 gap-4 flex-1 flex items-center justify-center hover:bg-accent hover:text-accent-foreground">
-                        <Icon name="simple-line-icons:pencil" size="24"/>
-                        Logs
-                    </a>
-                </div>
-            </div>
-        </div>
+  <div class="container mx-auto text-center py-16">
+    <h1 class="text-4xl font-bold mb-4">Добро пожаловать в GreDDBot</h1>
+    <p class="text-lg text-muted-foreground mb-8">
+      Управляйте вашим Twitch-ботом с помощью удобного интерфейса.
+    </p>
+    <div class="flex justify-center space-x-4">
+      <Button variant="outline">
+        <a href="/commands" class="btn btn-primary">Команды</a>
+      </Button>
+      <Button variant="outline">
+        <a href="/logs" class="btn btn-primary">Логи</a>
+      </Button>
+      <Button variant="outline" disabled>
+        <a href="/" class="btn btn-secondary">Личный кабинет</a>
+      </Button>
     </div>
   </div>
 </template>
