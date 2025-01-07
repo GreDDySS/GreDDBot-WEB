172215e8f48524da6c062cc964f47ffcac9cf6be
GreDDySS <misha.sudos@yandex.ru>
2025-01-07 20:53:29 +0800

    Footer

---
+++ components/common/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="bg-card text-card-foreground py-4 shadow-inner">
+    <div class="container mx-auto text-center">
+      <p>© 2025 GreDDBot. Все права защищены.</p>
+    </div>
+  </footer>
+</template>
