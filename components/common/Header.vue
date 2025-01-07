63c0cafd3cd283a76c6666c9ea2815913d467237
GreDDySS <misha.sudos@yandex.ru>
2025-01-07 20:53:04 +0800

    New Header component

---
+++ components/common/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="bg-card text-card-foreground shadow-md">
+    <nav class="container mx-auto flex justify-between py-4 px-6">
+      <h1 class="text-lg font-bold">GreDDBot</h1>
+      <ul class="flex space-x-4">
+        <li><NuxtLink to="/">Главная</NuxtLink></li>
+        <li><NuxtLink to="/commands">Команды</NuxtLink></li>
+        <li><NuxtLink to="/logs">Логи</NuxtLink></li>
+        <li><NuxtLink to="/">Документация</NuxtLink></li>
+        <li><NuxtLink to="/">Личный кабинет</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
