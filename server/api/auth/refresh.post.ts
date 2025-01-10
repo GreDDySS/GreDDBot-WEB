8ea38dce1536f2e77dcc22ce4e0a71c3ff7000bc
GreDDySS <misha.sudos@yandex.ru>
2025-01-10 18:19:18 +0800

    fix parsing refresh token

--- server/api/auth/refresh.post.ts
+++ server/api/auth/refresh.post.ts
@@ -1,18 +1,26 @@
 import type { AuthTokens } from "../../types/types"
-import { createJWT } from "~/server/utils/jwt"
+import { createJWT, verifyJWT } from "~/server/utils/jwt"
 
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig()
-  const { refresh_token } = await readBody(event)
+  const config = useRuntimeConfig();
+  const sessionCookie = getCookie(event, 'session');
 
-  const params = new URLSearchParams({
-    grant_type: 'refresh_token',
-    refresh_token,
-    client_id: config.public.TWITCH_CLIENT_ID,
-    client_secret: config.TWITCH_CLIENT_SECRET
-  })
-
+  if (!sessionCookie) {
+    throw createError({
+      statusCode: 401,
+      message: 'No session found'
+    })
+  }
+  
   try {
+    const { refresh_token } = verifyJWT(sessionCookie, config.JWT_SECRET)
+
+    const params = new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token,
+      client_id: config.public.TWITCH_CLIENT_ID,
+      client_secret: config.TWITCH_CLIENT_SECRET
+    })
     const response = await fetch('https://id.twitch.tv/oauth2/token', {
       method: 'POST',
       body: params
@@ -36,6 +44,7 @@ export default defineEventHandler(async (event) => {
 
     return { success: true }
   } catch (error) {
+    deleteCookie(event, 'session')
     throw createError({
       statusCode: 400,
       message: 'Failed to refresh token'
