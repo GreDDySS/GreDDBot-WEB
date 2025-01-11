cf0c8fec775c01e3cc650e344ee8940401365672
GreDDySS <misha.sudos@yandex.ru>
2025-01-12 02:29:15 +0800

    new module load

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
   shadcn: {
     prefix: '',
     componentDir: './components/ui/'
-  },
-  runtimeConfig: {
-    TWITCH_CLIENT_SECRET: process.env.NUXT_TWITCH_CLIENT_SECRET,
-    JWT_SECRET: process.env.JWT_SECRET,
-    public: {
-      TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
-      TWITCH_REDIRECT_URI: process.env.TWITCH_REDIRECT_URI
-    }
   }
-
 })
