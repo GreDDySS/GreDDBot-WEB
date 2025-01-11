13cbef10d20b97fbe155157e492c87ac05bc1e6e
GreDDySS <misha.sudos@yandex.ru>
2025-01-12 02:28:02 +0800

    redesign main page

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,26 @@ import {Button} from "../components/ui/button"
 </script>
 
 <template>
-  <div class="container mx-auto text-center py-16">
-    <h1 class="text-4xl font-bold mb-4">Добро пожаловать в GreDDBot</h1>
-    <p class="text-lg text-muted-foreground mb-8">
-      Управляйте вашим Twitch-ботом с помощью удобного интерфейса.
-    </p>
-    <div class="flex justify-center space-x-4">
-      <Button variant="outline" as-child>
-        <a href="/commands" class="btn btn-primary">Команды</a>
-      </Button>
-      <Button variant="outline" as-child>
-        <a href="/logs" class="btn btn-primary">Логи</a>
-      </Button>
-      <Button variant="outline" as-child disabled>
-        <a href="/dashboard" class="btn btn-secondary">Личный кабинет</a>
-      </Button>
-    </div>
+  <div class="container pt-32">
+    <!-- Content -->
+      <div class="max-w-6xl mx-auto text-center">
+        <h1 class="text-3xl md:text-5xl font-bold mb-6 bg-gradient-to-r from-white to-white/80 bg-clip-text text-transparent">
+          Добро пожаловать в GreDDBot
+        </h1>
+        <p class="text-xl md:text-2xl text-white/80 mb-8 max-w-3xl mx-auto">
+          Управляйте Twitch-ботом с помощью удобного интерфейса. 
+        </p>
+      </div>
+      <div class="flex justify-center space-x-4">
+        <Button variant="outline" as-child>
+        <a href="/commands">Команды</a>
+        </Button>
+        <Button variant="outline" as-child>
+          <a href="/logs">Логи</a>
+        </Button>
+        <!-- <Button variant="outline" as-child>
+          <a  href="/">Личный кабинет</a>
+        </Button> -->
+      </div>
   </div>
 </template>
